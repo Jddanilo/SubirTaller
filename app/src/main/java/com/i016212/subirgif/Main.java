0b0352d7c49739a49327dba91ed708c8da86f7cb
Jddanilo <jddanilo1@gmail.com>
2017-09-05 14:18:56 -0500

    Initial commit

---
+++ app/src/main/java/com/i016212/subirgif/Main.java
@@ -0,0 +1,13 @@
+package com.i016212.subirgif;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Main extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
